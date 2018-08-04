593059e0065498cd2988eede5a1c322a2a63feb2
Kilipurt <proandrey15@gmail.com>
2018-08-04 22:42:22 +0300

    lesson5

--- src/main/java/lesson5/main/Demo.java
+++ src/main/java/lesson5/main/Demo.java
@@ -1,4 +1,4 @@
-package lesson5;
+package lesson5.main;
 
 import org.hibernate.Session;
 
--- src/main/java/lesson5/main/Product.java
+++ src/main/java/lesson5/main/Product.java
@@ -1,4 +1,4 @@
-package lesson5;
+package lesson5.main;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;

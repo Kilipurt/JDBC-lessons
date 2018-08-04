21cde042b40f3d63a4958e7f7d39f51a6e872c6a
Kilipurt <proandrey15@gmail.com>
2018-08-04 22:27:22 +0300

    lesson5

---
+++ src/main/java/lesson5/simpleProductRepository/Demo.java
@@ -0,0 +1,27 @@
+package lesson5.simpleProductRepository;
+
+import lesson5.Product;
+
+public class Demo {
+    public static void main(String[] args) {
+        ProductRepository productRepository = new ProductRepository();
+
+//        Product product1 = new Product();
+//        product1.setId(1);
+//        product1.setName("save test");
+//        product1.setDescription("product for save");
+//        product1.setPrice(70);
+//
+//        productRepository.save(product1);
+
+//        Product product2 = new Product();
+//        product2.setId(1);
+//        product2.setName("update test");
+//        product2.setDescription("product for update");
+//        product2.setPrice(70);
+//
+//        productRepository.update(product2);
+
+        productRepository.delete(1);
+    }
+}
---
+++ src/main/java/lesson5/simpleProductRepository/ProductRepository.java
@@ -0,0 +1,37 @@
+package lesson5.simpleProductRepository;
+
+import lesson5.HibernateUtils;
+import lesson5.Product;
+import org.hibernate.Session;
+
+public class ProductRepository {
+    public void save(Product product) {
+        Session session = new HibernateUtils().createSessionFactory().openSession();
+        session.getTransaction().begin();
+
+        session.save(product);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(long id) {
+        Session session = new HibernateUtils().createSessionFactory().openSession();
+        session.getTransaction().begin();
+
+        session.delete(session.get(Product.class, id));
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void update(Product product) {
+        Session session = new HibernateUtils().createSessionFactory().openSession();
+        session.getTransaction().begin();
+
+        session.update(product);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+}
